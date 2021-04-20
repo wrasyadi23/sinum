92dd1bb76cb60a688192f8239f130f776b5752fe
Henda Bastian <hendabast@gmail.com>
2021-04-20 08:24:09 +0700

    fix(layout): fix side menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,9 +86,9 @@
 </div>
 
 {{-- <script src="{{ mix('js/app.js') }}" defer></script> --}}
-<script src="{{ asset('adminlte/dist/js/adminlte.min.js')}}"></script>
 <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
 <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ asset('adminlte/dist/js/adminlte.min.js')}}"></script>
 
 @yield('third_party_scripts')
 
